acca5a593223aae75bc2b1afabaceafa0a29d3ea
Grigory Mitrofanov <grelm.wolski@gmail.com>
2026-06-14 10:39:17 +0300

    chore(design): Добавлены токены с цветами в style.css. Добавлен шрифт Nunito, добавлен новый бэкграунд .scene

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,12 @@ const route = useRoute()
 </script>
 
 <template>
-  <div class="relative gap-y-2 justify-center items-center min-h-screen bg-background py-16 px-8">
-    <BackButtonComponent v-if="route.path !== '/'" />
-    <div class="flex w-screen justify-center mx-auto mt-8">
-      <router-view />
+  <div class="scene">
+    <div>
+      <BackButtonComponent v-if="route.path !== '/'" />
+      <div class="flex w-screen justify-center mx-auto mt-8">
+        <router-view />
+      </div>
     </div>
   </div>
 </template>
